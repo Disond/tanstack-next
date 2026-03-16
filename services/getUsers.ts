47aa7d8de408f7025f88e73b02df7798c5fc4baa
Disond <milosevic.petar4@gmail.com>
2026-03-16 21:01:33 +0100

    CR proposal fixes

--- services/getUsers.ts
+++ services/getUsers.ts
@@ -6,8 +6,9 @@ export type User = {
 
 // services/getUsers.ts - REAL DATA (safe way)
 export async function getUsers(): Promise<User[]> {
-    // API route (koji već radi!)
-    const res = await fetch("http://localhost:3000/api/users", {
+    const API_BASE_URL = process.env.APP_URL || "http://localhost:3000";
+
+    const res = await fetch(`${API_BASE_URL}/api/users`, {
         cache: "no-store",
     });
     if (!res.ok) throw new Error("Failed to fetch");
